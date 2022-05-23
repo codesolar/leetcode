6a71f2aa1b470f8fc5f8efbceab24079c3c77a5e
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-23 11:07:55 +0530

    Time: 22 ms (24.96%), Space: 71.4 MB (18.58%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(mp.containsKey(nums[i])==true){
+                return true;
+            }
+            else{
+                mp.put(nums[i],1);
+            }
+        }
+        return false;
+    }
+}
