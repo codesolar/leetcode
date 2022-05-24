a65432c19050bd2ae07b901f35a35d3eb11c5b16
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-24 22:09:44 +0530

    Time: 48 ms (29.33%), Space: 68 MB (5.06%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,25 +1,23 @@
 class Solution {
 public int subarraySum(int[] nums, int k) {
-    
-    
-    //do again
-    
-    
-    Map<Integer,Integer> mymap = new HashMap<>();
-    int sum=0;
-    mymap.put(sum,1); // frequency of 0 is 1
-    int maxsubarr = 0;
+    Map<Integer,Integer> mp=new HashMap<>();
+    int maxsub=0,sum=0;
     for(int i=0;i<nums.length;i++){
-        sum += nums[i];
-        if(mymap.containsKey(sum-k))
-            maxsubarr += mymap.get(sum-k);
-        if(mymap.containsKey(sum)){
-            int fre = mymap.get(sum)+1;
-            mymap.put(sum,fre);
-        }else{
-            mymap.put(sum,1);
+        sum=sum+nums[i];
+        if(sum==k){
+            maxsub++;
+        }
+        if(mp.containsKey(sum-k)){
+            maxsub+=mp.get(sum-k);
+        }
+        if(!mp.containsKey(sum)){
+            mp.put(sum,1);
+        }
+        else{
+            mp.put(sum,mp.get(sum)+1);
         }
     }
-    return maxsubarr;
+    return maxsub;
+
 }
 }
