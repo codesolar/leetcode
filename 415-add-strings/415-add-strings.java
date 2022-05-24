13437b637818a2364e3b6b686c5ca08e1418d0c4
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-24 18:38:26 +0530

    Time: 35 ms (8.04%), Space: 72.3 MB (5.51%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -1,34 +1,31 @@
 class Solution {
     public String addStrings(String num1, String num2) {
+        //try again with string not stringbuilder
         int len1=num1.length();
         int len2=num2.length();
         int i=len1-1,j=len2-1,c=0,sum=0;
-        StringBuilder str=new StringBuilder();
+        String str=new String();
         while(i>=0 && j>=0){
                                sum=Integer.parseInt(String.valueOf(num1.charAt(i)))+Integer.parseInt(String.valueOf(num2.charAt(j)))+c;
             c=sum/10;
             sum=sum%10;
-            str=(new StringBuilder(Integer.toString(sum))).append(str);
+            str=Integer.toString(sum)+str;
             i--;
             j--;
         }
         if(i<0 && j>=0){
-            System.out.println("if"+j);
             while(j>=0 && c>0){
                 sum=c+Integer.parseInt(String.valueOf(num2.charAt(j)));
                 c=sum/10;
                 sum=sum%10;
-                str=(new StringBuilder(Integer.toString(sum))).append(str);
+                str=Integer.toString(sum)+str;
                 j--;
-                   System.out.println("while");
             }
             if(j<0 && c>0){
-                str=(new StringBuilder("1")).append(str);
-                   System.out.println("2nd if");
+                str="1"+str;
             }
             else if(j>=0 && c<=0){
-                str=(new StringBuilder(num2.substring(0,j+1))).append(str);
-                   System.out.println("else if");
+                str=num2.substring(0,j+1)+str;
             }
         }
         else if(i>=0 && j<0){
@@ -36,19 +33,19 @@ class Solution {
                 sum=c+Integer.parseInt(String.valueOf(num1.charAt(i)));
                 c=sum/10;
                 sum=sum%10;
-                str=(new StringBuilder(Integer.toString(sum))).append(str);
+                str=Integer.toString(sum)+str;
                 i--;
             }
             if(i<0 && c>0){
-                str=(new StringBuilder("1")).append(str);
+                str="1"+str;
             }
             else if(i>=0 && c<=0){
-                str=(new StringBuilder(num1.substring(0,i+1))).append(str);
+                str=num1.substring(0,i+1)+str;
             }
         }
         else if(i<0 && j<0 && c>0){
-            str=(new StringBuilder("1")).append(str);
+            str="1"+str;
         }
-        return str.toString();
+        return str;
     }
 }
