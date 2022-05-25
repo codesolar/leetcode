0cf27174a380a7e524a086d19ed6714a0cc2dce9
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-25 19:46:16 +0530

    Time: 15 ms (18.92%), Space: 57.3 MB (60.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,28 +1,45 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int re=0,ce=0;
+//         boolean flag=false;
+//         for(int i=0;i<matrix[0].length;i++){
+//             ce++;
+//             if(target<matrix[0][i]){
+//                 break;
+//             }
+//         }
+//         for(int i=0;i<matrix.length;i++){
+//             re++;
+//             if(target<matrix[i][0]){
+//                 break;
+//             }
+//         }
+//         for(int i=0;i<re;i++){
+//             for(int j=0;j<ce;j++){
+//                 if(matrix[i][j]==target)
+//                 {
+//                     flag=true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return flag;
+//     }
+// }
+
+
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int re=0,ce=0;
-        boolean flag=false;
-        for(int i=0;i<matrix[0].length;i++){
-            ce++;
-            if(target<matrix[0][i]){
-                break;
-            }
+        
+        int r = 0, c = matrix[0].length - 1;
+        
+        while (r <= matrix.length -1 && c >= 0) {
+            if (matrix[r][c] == target) return true;
+            if (matrix[r][c] < target) r++;
+            else c--;
         }
-        for(int i=0;i<matrix.length;i++){
-            re++;
-            if(target<matrix[i][0]){
-                break;
-            }
-        }
-        for(int i=0;i<re;i++){
-            for(int j=0;j<ce;j++){
-                if(matrix[i][j]==target)
-                {
-                    flag=true;
-                    break;
-                }
-            }
-        }
-        return flag;
+        
+        return false;
     }
 }
