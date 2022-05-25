58ef3eda9241cd86b36c03157080903948d7be43
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-25 19:39:11 +0530

    Time: 60 ms (5.01%), Space: 57.7 MB (48.34%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int re=0,ce=0;
+        boolean flag=false;
+        for(int i=0;i<matrix[0].length;i++){
+            ce++;
+            if(target<matrix[0][i]){
+                break;
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            re++;
+            if(target<matrix[i][0]){
+                break;
+            }
+        }
+        System.out.print(re +" "+ce);
+        for(int i=0;i<re;i++){
+            for(int j=0;j<ce;j++){
+                if(matrix[i][j]==target)
+                {
+                    flag=true;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+}
