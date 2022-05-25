3c1eddb71c62aa066cd389a784faff4785ddbdae
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-25 19:47:19 +0530

    Time: 22 ms (7.22%), Space: 57 MB (62.13%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -29,17 +29,50 @@
 
 
 
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+        
+//         int r = 0, c = matrix[0].length - 1;
+        
+//         while (r <= matrix.length -1 && c >= 0) {
+//             if (matrix[r][c] == target) return true;
+//             if (matrix[r][c] < target) r++;
+//             else c--;
+//         }
+        
+//         return false;
+//     }
+// }
+
+
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
-        int r = 0, c = matrix[0].length - 1;
-        
-        while (r <= matrix.length -1 && c >= 0) {
-            if (matrix[r][c] == target) return true;
-            if (matrix[r][c] < target) r++;
-            else c--;
+        for (int[] row : matrix) {
+            int index = search(row, target);
+            if (index >= 0) {
+                return true;
+            }
         }
         
         return false;
+    }
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return -1;
     }
 }
