cbb8c80526ef65fa6f38decb448bb38e585e46ca
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-25 16:22:11 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (27.65%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -5,20 +5,13 @@ class Solution {
         int t=0,t1=0;
         while(r>=2){
         for(int l=0;l<r-1;l++){
-        t=matrix[i+l][j+r-1];
-        matrix[i+l][j+r-1]=matrix[i][j+l];
-        t1=matrix[i+r-1][j+r-1-l];
-        matrix[i+r-1][j+r-1-l]=t;
-        t=matrix[i+r-1-l][j];
-        matrix[i+r-1-l][j]=t1;
-        matrix[i][j+l]=t;
-            System.out.println("r="+r);
-            for(int x=0;x<matrix.length;x++){
-                for(int y=0;y<matrix.length;y++){
-                    System.out.print(matrix[x][y]+" ");
-                }
-                System.out.println();
-            }
+            t=matrix[i+l][j+r-1];
+            matrix[i+l][j+r-1]=matrix[i][j+l];
+            t1=matrix[i+r-1][j+r-1-l];
+            matrix[i+r-1][j+r-1-l]=t;
+            t=matrix[i+r-1-l][j];
+            matrix[i+r-1-l][j]=t1;
+            matrix[i][j+l]=t;
         }
             r=r-2;
             i+=1;
