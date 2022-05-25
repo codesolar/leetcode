ecba8267f986df32cbc83ae58e3435560da04d98
souradipta20 <55339877+souradipta20@users.noreply.github.com>
2022-05-25 19:48:18 +0530

    Time: 35 ms (5.01%), Space: 58 MB (32.47%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,45 +1,45 @@
-// class Solution {
-//     public boolean searchMatrix(int[][] matrix, int target) {
-//         int re=0,ce=0;
-//         boolean flag=false;
-//         for(int i=0;i<matrix[0].length;i++){
-//             ce++;
-//             if(target<matrix[0][i]){
-//                 break;
-//             }
-//         }
-//         for(int i=0;i<matrix.length;i++){
-//             re++;
-//             if(target<matrix[i][0]){
-//                 break;
-//             }
-//         }
-//         for(int i=0;i<re;i++){
-//             for(int j=0;j<ce;j++){
-//                 if(matrix[i][j]==target)
-//                 {
-//                     flag=true;
-//                     break;
-//                 }
-//             }
-//         }
-//         return flag;
-//     }
-// }
-
-
-//not my solution
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
-        int r = 0, c = matrix[0].length - 1;
-        
-        while (r <= matrix.length -1 && c >= 0) {
-            if (matrix[r][c] == target) return true;
-            if (matrix[r][c] < target) r++;
-            else c--;
+        int re=0,ce=0;
+        boolean flag=false;
+        for(int i=0;i<matrix[0].length;i++){
+            ce++;
+            if(target<matrix[0][i]){
+                break;
+            }
         }
-        
-        return false;
+        for(int i=0;i<matrix.length;i++){
+            re++;
+            if(target<matrix[i][0]){
+                break;
+            }
+        }
+        for(int i=0;i<re;i++){
+            for(int j=0;j<ce;j++){
+                if(matrix[i][j]==target)
+                {
+                    flag=true;
+                    break;
+                }
+            }
+        }
+        return flag;
     }
 }
+
+
+// //not my solution
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+        
+//         int r = 0, c = matrix[0].length - 1;
+        
+//         while (r <= matrix.length -1 && c >= 0) {
+//             if (matrix[r][c] == target) return true;
+//             if (matrix[r][c] < target) r++;
+//             else c--;
+//         }
+        
+//         return false;
+//     }
+// }
