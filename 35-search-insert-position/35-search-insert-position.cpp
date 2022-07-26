a142674604dacc5baf9896304513e5c699fe561a
SOURADIPTA CHOUDHURI <55339877+codesolar@users.noreply.github.com>
2022-07-26 16:11:00 +0530

    Time: 9 ms (36.84%), Space: 9.7 MB (72.01%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int binary_search(vector<int> &nums,int l,int r,int x,int y){
+        if(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==x){
+                return mid;
+            }
+            else if(nums[mid]>x){
+                y=0;
+                return binary_search(nums,l,mid-1,x,y);//r
+            }
+            else{
+                y=1;
+                return binary_search(nums,mid+1,r,x,y);//l
+            }
+        }
+        return l;
+    }
+    int searchInsert(vector<int>& nums, int target) {
+        return binary_search(nums,0,nums.size()-1,target,-1);
+    }
+};
