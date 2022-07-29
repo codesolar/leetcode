a089080904a6e594bbe3ea8da1d0187059c34d3d
SOURADIPTA CHOUDHURI <55339877+codesolar@users.noreply.github.com>
2022-07-29 15:28:02 +0530

    Time: 1033 ms (5.09%), Space: 14.3 MB (25.30%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,42 +7,34 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
-public:
-    unordered_map<TreeNode*,TreeNode*> reverse_graph;
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        inorder(root);
-        unordered_map<TreeNode*,int> map;
-        TreeNode* key=p;
-        while(key!=root){
-            if(map.find(key)==map.end()){
-                map.insert({key,1});
-                key=reverse_graph[key];
-            }
+    bool findOnSubTree(TreeNode* root, TreeNode* q) {
+
+        if (root) {
+            return findOnSubTree(root->left, q) or (root->val == q->val) or findOnSubTree(root->right, q);
         }
-        map.insert({root,1});
-        key=q;
-        while(key!=root){
-            if(map.find(key)!=map.end()){
-                //no need to insert ,just check if key is present 
-                break;
-            }
-            key=reverse_graph[key];
-        }
-        //here key can be a number which is already present or key can be root->val
-        return key;
+
+        return false;
     }
-    void inorder(TreeNode* root){
-        if(root){
-            inorder(root->left);
-            if(root->left and reverse_graph.find(root->left)==reverse_graph.end()){
-                reverse_graph.insert({root->left,root});
-            }
-            if(root->right and reverse_graph.find(root->right)==reverse_graph.end()){
-                reverse_graph.insert({root->right,root});
-            }
-            inorder(root->right);
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // check if both in the same tree or not?
+        // recursively go to that subtree only
+        if (findOnSubTree(root->left, p) and findOnSubTree(root->left, q)) {
+            return lowestCommonAncestor(root->left, p , q);
+        } else if (findOnSubTree(root->right, p) and findOnSubTree(root->right, q)) {
+            return lowestCommonAncestor(root->right, p , q);
         }
-        
+
+        // now check if both are in different subtree or not?
+        if (findOnSubTree(root->left, p) and findOnSubTree(root->right, q)) {
+            return root;
+        } else if (findOnSubTree(root->right, p) and findOnSubTree(root->left, q)) {
+            return root;
+        }
+
+        // base case all must have the ancestor root
+        return root;
     }
 };
