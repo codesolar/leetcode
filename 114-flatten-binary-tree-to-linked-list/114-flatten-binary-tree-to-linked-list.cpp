47c5d868bf56fec76697683a4d2cb046c5916bd3
SOURADIPTA CHOUDHURI <55339877+codesolar@users.noreply.github.com>
2022-07-29 12:46:46 +0530

    Time: 8 ms (67.50%), Space: 12.8 MB (18.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -16,17 +16,14 @@ public:
         if(root==NULL) return;
         if(root->left==NULL and root->right==NULL) return;
         flattenLeft(root,root);
-        cout<<root->val;
         flattenRight(root);
         return;
     }
     TreeNode* findlast(TreeNode* root){
-        cout<<"root-"<<root->val;
         while(root->left){
             root=root->left;
             
         }
-        cout<<root->val<<endl;
         return root;
     }
     void flattenLeft(TreeNode* root,TreeNode* last){
@@ -36,11 +33,10 @@ public:
          
             flattenLeft(root->right,root->right);
                last=findlast(last);
-            cout<<"in-"<<root->val<<endl;
+
             last->left=root->right;
             root->right=NULL;
-            if(root->left!=NULL)
-            cout<<root->left->val<<"after"<<endl;
+      
         }
     }
     void flattenRight(TreeNode* root){
