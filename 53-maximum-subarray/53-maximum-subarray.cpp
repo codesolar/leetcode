9f6e874e1c6d2f3d2dcdec8aff882dada6eb1a6c
SOURADIPTA CHOUDHURI <55339877+codesolar@users.noreply.github.com>
2022-07-30 20:10:27 +0530

    Time: 174 ms (49.11%), Space: 67.8 MB (10.91%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -5,20 +5,13 @@ public:
         int i=0;
         for(int j=0;j<nums.size();j++){
             sum+=nums[j];
-            if(sum<nums[j]){
-                i=j;
-                sum=nums[j];
-                
-            }
-            else if(sum<0 and sum<maxsum){
-                i=j+1;
-                sum=0;
-                continue;
-            }
             if(sum>maxsum){
                 maxsum=sum;
             }
-            
+            if(sum<0){
+                sum=0;
+                i=j+1;
+            }
         }
         return maxsum;
     }
