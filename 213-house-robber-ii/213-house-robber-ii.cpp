e2292155147a09c56b086990d1b14d55a143f929
SOURADIPTA CHOUDHURI <55339877+codesolar@users.noreply.github.com>
2022-08-02 01:29:11 +0530

    Time: 12 ms (5.86%), Space: 9.2 MB (6.76%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    unordered_map<int,int> map;
+    int moneyRob(vector<int>& nums,int n){
+     //   cout<<"n="<<n<<endl;
+        if(map.find(n)!=map.end()){
+            return map[n];
+        }
+        if(n==0){
+            if(map.find(n)==map.end())
+                map.insert({0,0});
+            return map[n];
+        }
+        if(n==1){
+            if(map.find(n)==map.end())
+            map.insert({1,nums[n-1]});
+            return map[n];
+        }
+        if(n==2){
+            if(map.find(n)==map.end())
+            map.insert({2,max(nums[n-2],nums[n-1])});
+            return map[n];
+        }
+        
+        int w1=nums[n-1]+moneyRob(nums,n-2);
+        int w2=nums[n-2]+moneyRob(nums,n-3);
+        map.insert({n,max(w1,w2)});
+        return map[n];
+        
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1){
+            return nums[0];
+        }
+        vector<int> v1;
+        for(int i=0;i<nums.size()-1;i++){
+            v1.push_back(nums[i]);
+        }
+        for(int i=0;i<v1.size();i++){
+            cout<<v1[i];
+        }
+        int w1=moneyRob(v1,v1.size());
+        vector<int> v2;
+        for(int i=1;i<nums.size();i++){
+            v2.push_back(nums[i]);
+        }
+         for(int i=0;i<v2.size();i++){
+            cout<<v2[i];
+        }
+        cout<<w1;
+        map.clear();
+        int w2=moneyRob(v2,v2.size());
+        cout<<w2;
+        return max(w1,w2);
+    }
+};
+
