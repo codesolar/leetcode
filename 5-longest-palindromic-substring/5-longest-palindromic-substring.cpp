76de519bef079dba5f4c8db5c0141976bdf0837c
SOURADIPTA CHOUDHURI <55339877+codesolar@users.noreply.github.com>
2022-09-04 12:21:53 +0530

    Time: 450 ms (28.00%), Space: 11.2 MB (60.18%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -16,7 +16,7 @@ public:
             }
         }
         
-        // cout<<end<<"H";
+      
         for(int i=0;i<s.length()-1;i++){
             if(s.at(i)==s.at(i+1)){
                 dp[i][i+1]=1;
@@ -27,22 +27,6 @@ public:
         }
         
         
-        // cout<<end<<"H";
-        // cout<<s.length();
-        // for(int i=0;i+2<s.length();i++){
-        //     // cout<<end<<"H";
-        //     for(int j=i+2;j<s.length();j++){
-        //         if((s.at(i)==s.at(j)) and (dp[i+1][j-1]==1)){
-        //             dp[i][j]=1;
-        //             end=j;
-        //             start=i;
-        //             cout<<start<<endl;
-        //             cout<<end<<endl;
-        //         }
-        //     }
-        // }
-        
-        
         for(int l=3;l<=s.length();l++){
             for(int i=0;i<=s.length()-l;i++){
                 int j=i+l-1;
@@ -50,12 +34,10 @@ public:
                     dp[i][j]=1;
                     end=j;
                     start=i;
-                    // cout<<start<<endl;
-                    // cout<<end<<endl;
+              
                 }
             }
         }
-        // cout<<end<<"H";
         return s.substr(start,end-start+1);
         
     }
